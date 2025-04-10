340123edda63bee4fcccf8dfc54c3217109ab347
Sunil Gopal C V <sunilgopal63@gmail.com>
2025-04-10 09:54:12 +0530

    package conflict resolved

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: import.meta.env.VITE_BASE_PATH || "/nlp-code-sprint",
 });
